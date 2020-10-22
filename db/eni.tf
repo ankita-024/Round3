94e3de5716675f4aeb9ad1b0afa4863fcd591702
Ankita Anand <73189096+ankita-024@users.noreply.github.com>
2020-10-23 03:36:53 +0530

    Update eni.tf

--- db/eni.tf
+++ db/eni.tf
@@ -1,1 +1,11 @@
+module "db_eni"{
+  
 
+  subnet_id = "${data.aws_subnet.id}"
+
+  security_group = []
+  
+
+  name                       = "${var.stack_name}-db-${var.tag_environment}"
+  tag_team                   = "${var.tag_team}"
+} 
