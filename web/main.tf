aa823021b93fde77adc115128b7d6badda12bb73
Ankita Anand <73189096+ankita-024@users.noreply.github.com>
2020-10-23 03:20:50 +0530

    Update main.tf

--- web/main.tf
+++ web/main.tf
@@ -1,7 +1,18 @@
 
 #______________________________________AMI______________________________________
 
-data "aws_ami" "app_ami" {
+data "aws_ami" "web_ami" {
+  most_recent = true
+  filter {
+    name   = "tag:Name"
+    values = ["${var.app_ami_name}"]
+  }
+  filter {
+    name   = "tag:Lifecycle"
+    values = ["authorised"]
+  }
+} 
+data "aws_ami" "web_ami_external" {
   most_recent = true
   filter {
     name   = "tag:Name"
@@ -21,12 +32,12 @@ data "template" "userdata" {
     vault_environment       = "${var.tag_environment}"
     tag_environment         = "${var.tag_environment}"
     vault_addr              = "${var.vault_addr}"
-    vault_app_role          = "${var.vault_app_role}"
+    vault_app_role          = "${var.vault_web_role}"
     domain_join_user        = "${var.domain_join_user}"
     domain_join_password    = "${var.domain_join_password}"
-    ou_path                 = "${var.app_ou_path}"
-    myappear_location       = "${var.myappear_location}"
-    asg_name                = "${var.asg_app_name}"
+    ou_path                 = "${var.web_ou_path}"
+    myappear_location       = "${var.myappzip_location}"
+    asg_name                = "${var.asg_web_name}"
     logs_bucket_name        = "${var.logs_bucket_name}"
     no_proxy                = "${var.no_proxy}"
     admin_group      = "${var.admin_group}"
@@ -37,44 +48,43 @@ data "template" "userdata" {
  
 #____________________________Data Source For subnets_________________________
 
-#____________________________Application load balancer_________________________
+#____________________________Application load balancer internal_________________________
 
-resource "aws_lb" "app_alb" {
+resource "aws_lb" "web_alb" {
   provider = "aws.client"
-  security_groups            =  ["${data.terraform_remote_state.backend_sg.app_alb_sg_id}",
-    "${data.terraform_remote_state.shared_sg.shared_app_alb_to_app_asg_sg_id}",
+  security_groups            =  []
   ]
   subnets                    = ["${data.aws_subnet.*.id}"]
   load_balancer_type         = "application"
   internal                   =  true
-  name                       = "${var.stack_name}-app-ALB-${var.tag_environment}"
+  name                       = "${var.stack_name}-web-ALB-${var.tag_environment}"
   idle_timeout               = "${var.idle_timeout}" 
    tags {
-   Name                       = "${var.stack_name}-app-ALB-${var.tag_environment}"
+   Name                       = "${var.stack_name}-web-ALB-${var.tag_environment}"
    tag_environment            = "${var.tag_environment}"
    team                       = "${var.tag_team}"
  }
 } 
 
-resource "aws_lb_target_group" "app_tg" {
+resource "aws_lb_target_group" "web_tg" {
   provider ="aws.client"
   vpc_id   = "${var.vpc_id}" 
-  name                       = "${var.stack_name}-app-ALB-TG-${var.tag_environment}"
+  name                       = "${var.stack_name}-web-ALB-TG-${var.tag_environment}"
   port                       = "${var.port_7070}"                
   protocol                   = "${var.protocol_http}"
-  deregistration_delay       = "${var.app_tg_deregistration_delay}"
+  deregistration_delay       = "${var.web_tg_deregistration_delay}"
   stickiness {
     type = "lb_cookie"
     enabled = true
-    cookie_duration = "${var.app_tg_cookie_duration}"
+    cookie_duration = "${var.web_tg_cookie_duration}"
   }
 
   health_check {
-    interval            = "${var.app_tg_check_interval}"
+    interval            = "${var.web_tg_check_interval}"
     path                = ""
     timeout             = ""
-    healthy_threshold   = "${var.app_tg_healthy_threshold}"
-    unhealthy_threshold = "${var.app_tg_unhealthy_threshold}"
+    healthy_threshold   = "${var.web_tg_healthy_threshold}"
+    unhealthy_threshold = "${var.web_tg_unhealthy_threshold}"
     matcher             = ""
   }
   tags {
@@ -82,37 +92,150 @@ resource "aws_lb_target_group" "app_tg" {
   }
 }  
  
-resource "aws_lb_listener" "app_alb_listener" {
+resource "aws_lb_listener" "web_alb_listener" {
   provider = "aws.client"
-  load_balancer_arn = "${aws_lb.app_alb.arn}"
+  load_balancer_arn = "${aws_lb.web_alb.arn}"
   protocol          = "HTTPS"
   port            = "${var.port_cccc}"
   ssl_policy      = "${var.listener_ssl_policy}"
   certificate_arn = "${var.listener_certificate_arn}"
   default_action {
-    target_group_arn = "${aws_lb_target_group.app_tg.arn}"
+    target_group_arn = "${aws_lb_target_group.web_tg.arn}"
     type             = "forward"
   }
 }
 
-resource "aws_lb_listener_rule" "app_alb_listener_rule" {
+resource "aws_lb_listener_rule" "web_alb_listener_rule" {
   
   provider = "aws.client"
 
-  listener_arn     = "${aws_lb_listener.app_alb_listener.arn}"
-  priority         = "${var.app_priority_9999}"
+  listener_arn     = "${aws_lb_listener.web_alb_listener.arn}"
+  priority         = "${var.web_priority_9999}"
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.app_tg.arn}"
+    target_group_arn = "${aws_lb_target_group.web_tg.arn}"
   }
   condition {
-    field  = "${var.app_listener_rule_field}"
-    values = ["${var.app_listener_rule_values}"]
+    field  = "${var.web_listener_rule_field}"
+    values = ["${var.web_listener_rule_values}"]
   }
 } 
+#____________________________Application load balancer external_________________________
+
+resource "aws_lb" "web_alb_external" {
+  provider = "aws.client"
+  security_groups            =  []
+  ]
+  subnets                    = ["${data.aws_subnet.*.id}"]
+  load_balancer_type         = "application"
+  internal                   =  true
+  name                       = "${var.stack_name}-web-ALB-ext-${var.tag_environment}"
+  idle_timeout               = "${var.idle_timeout}" 
+   tags {
+   Name                       = "${var.stack_name}-web-ALB-ext-${var.tag_environment}"
+   tag_environment            = "${var.tag_environment}"
+   team                       = "${var.tag_team}"
+ }
+} 
+
+resource "aws_lb_target_group" "web_tg_ext" {
+  provider ="aws.client"
+  vpc_id   = "${var.vpc_id}" 
+  name                       = "${var.stack_name}-web-ALB-TG-ext-${var.tag_environment}"
+  port                       = "${var.port_7070}"                
+  protocol                   = "${var.protocol_http}"
+  deregistration_delay       = "${var.web_tg_deregistration_delay}"
+  stickiness {
+    type = "lb_cookie"
+    enabled = true
+    cookie_duration = "${var.web_tg_cookie_duration}"
+  }
+
+  health_check {
+    interval            = "${var.web_tg_check_interval}"
+    path                = ""
+    timeout             = ""
+    healthy_threshold   = "${var.web_tg_healthy_threshold}"
+    unhealthy_threshold = "${var.web_tg_unhealthy_threshold}"
+    matcher             = ""
+  }
+  tags {
+.....
+  }
+}  
  
+resource "aws_lb_listener" "web_alb_listener_ext" {
+  provider = "aws.client"
+  load_balancer_arn = "${aws_lb.web_alb.arn}"
+  protocol          = "HTTPS"
+  port            = "${var.port_cccc}"
+  ssl_policy      = "${var.listener_ssl_policy}"
+  certificate_arn = "${var.listener_certificate_arn}"
+  default_action {
+    target_group_arn = "${aws_lb_target_group.web_tg.arn}"
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_listener_rule" "web_alb_listener_rule_ext" {
+  
+  provider = "aws.client"
+
+  listener_arn     = "${aws_lb_listener.web_alb_listener.arn}"
+  priority         = "${var.web_priority_9999}"
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.web_tg.arn}"
+  }
+  condition {
+    field  = "${var.web_listener_rule_field}"
+    values = ["${var.web_listener_rule_values}"]
+  }
+} 
+
 #_________________________ASG_________________________________
+resource "aws_autoscaling_group" "asg_external" {
+  # ... other configuration ...
+
+  lifecycle {
+    ignore_changes = [load_balancers, target_group_arns]
+  }
+}
+
+resource "aws_autoscaling_group" "asg_internal" {
+  # ... other configuration ...
+
+  lifecycle {
+    ignore_changes = [load_balancers, target_group_arns]
+  }
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment_bar" {
+  autoscaling_group_name = aws_autoscaling_group.asg.id
+  elb                    = aws_elb.test.id
+}
+ 
+resource "aws_autoscaling_schedule" "app_scheduler_up" {
+  provider = "aws.client"
+  scheduled_action_name  = "app_scheduler_up"
+  min_size               = "${var.app_scheduler_up_min_size}"
+  max_size               = "${var.app_scheduler_up_max_size}"
+  desired_capacity       = "${var.app_scheduler_up_desired_capacity}"
+  recurrence             = "${var.app_scheduler_up_recurrence}"
+  autoscaling_group_name = "${data.terraform_remote_state.backend.asg_name}"
+} 
+
+resource "aws_autoscaling_schedule" "app_scheduler_down" {
+  provider = "aws.client"
+  scheduled_action_name  = "app_scheduler_down"
+   min_size               = "${var.app_scheduler_down_min_size}"
+  max_size               = "${var.app_scheduler_down_max_size}"
+  desired_capacity       = "${var.app_scheduler_down_desired_capacity}"
+  recurrence             = "${var.app_scheduler_down_recurrence}"
+  autoscaling_group_name = "${data.terraform_remote_state.backend.asg_name}"
+} 
 
  
 
