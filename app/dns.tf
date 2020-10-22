1f19bd28d5908d582f2e85d8ae8650453d783389
Ankita Anand <73189096+ankita-024@users.noreply.github.com>
2020-10-23 02:29:18 +0530

    Update dns.tf

--- app/dns.tf
+++ app/dns.tf
@@ -1,1 +1,13 @@
-
+#____________________dns______________________________
+resource "aws_route53_record" "app_dns" {
+  provider 			  = "aws.management"
+  name                = "${var.service_record_name_app}.${var.hosted_zone}"
+  zone_id             = "${var.hosted_zone_id}"
+  type 				  = "A"
+  alias {
+    name                   = "${data.terraform_remote_state.backend.alb_dns_name}"
+    zone_id                = "${data.terraform_remote_state.backend.alb_zone_id}"
+    evaluate_target_health = true
+  }
+} 
+ 
